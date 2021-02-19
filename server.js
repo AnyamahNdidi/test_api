2a192196d542ff123b97f4e6098cf97ab6e2f375
AnyamahNdidi <theo4felix@gmail.com>
2021-02-19 12:06:02 +0100

    updated mongoose

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const mongoosebd = require("mongoose")
 const studentModel = require('./Model/model')
 // const PORT = 7600
 
+//connecting to mongodb atlas
+
+mongoosebd.connect(process.env.DATABASE_URL, {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+
+mongoosebd.connection.once('open', () => {
+  console.log('connected to database successfully....')
+}).on('error', () => {
+  console.log('Database connection failed')
+})
+
+
 // creatin an object instance
 const app = express()
 
@@ -14,5 +31,6 @@ app.use(express.json())
 // listen to the port
 app.listen(process.env.PORT, () => {
   console.log(`connecting to port  ${process.env.PORT}`)
+  console.log("sudyiosdvjhkdhjkk")
 })
 
